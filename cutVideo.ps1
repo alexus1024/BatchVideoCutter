4929d5bbd47c4d67ca47eac2a1050b5149df6d8b
Alexus <alexusr@nm.ru>
2015-05-12 03:43:07 +0500

    logo overlay feature

--- cutVideo.ps1
+++ cutVideo.ps1
@@ -1,9 +1,9 @@
-﻿$ffmpeg = "E:\Program_Files\ffmpeg-20140906-git-1654ca7-win64-static\bin\ffmpeg.exe"
-$rules = "F:\CamVideo\2014-08-25-09-01 ФИТ\Ричард тайминг для резки.txt"
+﻿$ffmpeg = "E:\Program_Files\ffmpeg-20140906-git-1654ca7-win64-static\bin\"
+$rules = "E:\dev\ffmpeg\testrules.xml"
 $latestFfmpegUrl32 = 'http://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.7z'
 $latestFfmpegUrl64 = 'http://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.7z'
 
-
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 chcp 65001
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
@@ -38,7 +38,7 @@ foreach($nodeRaw in Select-Xml  "/work/cut" $rules){
 
     $outDir = [System.IO.Path]::GetDirectoryName($nodeRaw.Node.output)
 
-    if(!(Test-Path $outDir)){
+    if($outDir -and !(Test-Path $outDir)){
         New-Item -ItemType directory -Path $outDir
 
         Write-Output "Создана директория $outDir"
@@ -47,6 +47,17 @@ foreach($nodeRaw in Select-Xml  "/work/cut" $rules){
 
 if($notFoundInput.Count -gt 0){
     Write-Error "Some input files not exist: $notFoundInput"
+}
+
+
+# проверка логотипа
+$logoInfo = Select-Xml  "/work[@logo]" $rules
+$logoFilename = $logoInfo.Node.logo
+
+if($logoFilename -and -not (Test-Path($logoFilename))){
+    Write-Error "Файл логотипа не найден. $logoFilename"
+}else{
+    Write-Output "Используется логотип из файла. $logoFilename"
 }
 
 
@@ -62,19 +73,36 @@ foreach($nodeRaw in Select-Xml  "/work/cut" $rules){
     }else{
         $len = $node.len;
     }
-    ,
-    $a = '-i', $node.input, '-ss', $node.from, '-c', 'copy', '-t', $len, '-acodec', 'copy', '-vcodec', 'copy', '-y', '-loglevel', 'error', $node.output
+    
+    if($logoFilename){
+    # вставка логотипа включена
+        $videoInfo = (& "$($ffmpeg)ffprobe.exe" -show_streams -of xml -loglevel quiet $node.input) | Out-String
+        $videoStreamInfo = Select-Xml -Content $videoInfo -XPath "/ffprobe/streams/stream[@codec_type='video' and @width and @height][1]"
 
+        $videoWidth = $videoStreamInfo.Node.width
+        $videoHeight = $videoStreamInfo.Node.height
+
+        $logoWidth = $videoWidth/10
+
+        $ffmpegParams = "-i", $node.input, "-i", $logoFilename, "-filter_complex", "[1]scale=$($logoWidth):$($logoWidth)/a [logo]; [0][logo]overlay=main_w-overlay_w-10:10", "-ss", $node.from, "-t", $len, "-y", "-loglevel", "error", $node.output
+
+    }else{
+        # если логотип не вставляем - включаем копирование потогов, чтобы не пережимать видео
+        $ffmpegParams = "-i", $node.input, "-ss", $node.from, "-c", "copy",  "-t", $len, "-y", "-loglevel", "error", $node.output    
+    }
     
+
     $error.Clear()
-    (& $ffmpeg $a)
     
-    if($error.Count -gt 0){
-        Write-Output "Ошибка при обработке задачи '$counter': $error"
+    try{
+        (& "$($ffmpeg)ffmpeg.exe" $ffmpegParams)
+        Write-Output "Задача $counter выполнена..."
+    }catch{
+        
+        Write-Warning "Задача $counter : Ошибка. $($_.Exception)"
         $cutErrors += $error
     }
-
-    Write-Output "Задача $counter выполнена..."
+    
 
     $counter++
 
@@ -83,5 +111,5 @@ foreach($nodeRaw in Select-Xml  "/work/cut" $rules){
 Write-Output "Готово!"
 
 if($cutErrors.Count -gt 0){
-    Write-Error "Was errors"
+    Write-Error "При выолнении задач возникали ошибки"
 }
